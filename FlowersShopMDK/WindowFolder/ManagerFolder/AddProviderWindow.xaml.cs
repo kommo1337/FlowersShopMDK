9c35b882c9ec8f99468dfa366dc5c9ddc4a6e9aa
Дмитрий Талалаев <k.kommo@yandex.ru>
2022-11-29 03:58:26 +0300

    addMenaga

--- FlowersShopMDK/WindowFolder/ManagerFolder/AddProviderWindow.xaml.cs
+++ FlowersShopMDK/WindowFolder/ManagerFolder/AddProviderWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FlowersShopMDK.ClassFolder;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,54 @@ namespace FlowersShopMDK.WindowFolder.ManagerFolder
     /// </summary>
     public partial class AddProviderWindow : Window
     {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-Q9BEC2K;Initial Catalog=kommo;Integrated Security=True");
+        SqlCommand SqlCommand;
         public AddProviderWindow()
         {
             InitializeComponent();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            new ManagerWindow().Show();
+            Close();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand =
+                    new SqlCommand("Insert Into " +
+                    "dbo.[Provider] (Name, NameProvider, SurnameProvider, TherdNameProvider, Phone)" +
+                    $"Values ('{TitleTb.Text}'," +
+                    $"'{NameTb.Text}'," +
+                    $"'{SureNameTb.Text}'," +
+                    $"'{TherdNameTb.Text}'," +
+                    $"'{Phone.Text}')", sqlConnection);
+                SqlCommand.ExecuteNonQuery();
+                MBClass.InfoMB($"Поставщик добавлен");
+            }
+            catch (Exception ex)
+            {
+
+                MBClass.ErrorMB(ex);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
         }
     }
 }
